dac5d4b9ff04de38d24e302b3e671f129494741c
lunacrafts <lunacrafts@protonmail.com>
2024-11-23 23:17:24 +0100

    Dirtbaggz.com

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Atom - A Tailus template';
-export const SITE_DESCRIPTION = 'Modern free Tailwind CSS, Astro landing page template for startups | built with Tailus Themer';
-export const SITE_URL = 'https://atom.tailus.io';
+export const SITE_TITLE = 'Dirtbaggz badass website';
+export const SITE_DESCRIPTION = 'Dirtbaggz.com FTW!!!!!!!11';
+export const SITE_URL = 'https://dirtbaggz.com';
 export const SITE_IMAGE = '/og.png';
